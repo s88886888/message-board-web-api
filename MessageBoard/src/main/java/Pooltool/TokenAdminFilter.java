67348844da1e71da1ae4229116e11a121f969981
Linson <854700937@qq.com>
2022-05-30 00:20:11 +0800

    紧急修复

--- MessageBoard/src/main/java/Pooltool/TokenAdminFilter.java
+++ MessageBoard/src/main/java/Pooltool/TokenAdminFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "AdminTokenFilter", urlPatterns = {"/selectuserbyname", "/SelectUser", "/selectuserbyid", "/updateServletbyadmin"})
+@WebFilter(filterName = "AdminTokenFilter", urlPatterns = {"/selectuserbyname", "/SelectUser", "/selectuserbyid", "/updateServletbyadmin","/deletearticle","/deleteuser"})
 public class TokenAdminFilter extends HttpFilter {
 
     @Override
